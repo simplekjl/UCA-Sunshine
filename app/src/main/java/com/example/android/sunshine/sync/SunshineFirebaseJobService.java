2d52b1ddc00c65391bb89ffd931067a438677c78
Jose L Crisostomo <jl.cs606@gmail.com>
2019-03-02 14:32:07 -0800

    exercise Done

--- app/src/main/java/com/example/android/sunshine/sync/SunshineFirebaseJobService.java
+++ app/src/main/java/com/example/android/sunshine/sync/SunshineFirebaseJobService.java
@@ -1,3 +1,12 @@
+package com.example.android.sunshine.sync;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+
+import com.firebase.jobdispatcher.JobParameters;
+import com.firebase.jobdispatcher.JobService;
+
 /*
  * Copyright (C) 2016 The Android Open Source Project
  *
@@ -13,13 +22,33 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// TODO (2) Make sure you've imported the jobdispatcher.JobService, not job.JobService
+public class SunshineFirebaseJobService extends JobService{
 
-// TODO (3) Add a class called SunshineFirebaseJobService that extends jobdispatcher.JobService
+    private AsyncTask<Void, Void, Void> mFetchWeatherTask;
 
-//  TODO (4) Declare an ASyncTask field called mFetchWeatherTask
+    @Override
+    public boolean onStartJob(final JobParameters job) {
+        mFetchWeatherTask = new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                Context context = getApplicationContext();
+                SunshineSyncTask.syncWeather(context);
+                return null;
+            }
 
-//  TODO (5) Override onStartJob and within it, spawn off a separate ASyncTask to sync weather data
-//              TODO (6) Once the weather data is sync'd, call jobFinished with the appropriate arguments
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                jobFinished(job,false);
+            }
+        };
+        return true;
+    }
 
-//  TODO (7) Override onStopJob, cancel the ASyncTask if it's not null and return true
+    @Override
+    public boolean onStopJob(@NonNull JobParameters job) {
+        if(mFetchWeatherTask != null){
+            mFetchWeatherTask.cancel(true);
+        }
+        return true;
+    }
+}
